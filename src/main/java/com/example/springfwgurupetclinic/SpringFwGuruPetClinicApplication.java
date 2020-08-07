e12ab8fdcdb9c1e17afd44f55a440c0828107488
oliver <dr.oliver.lang@gmail.com>
2020-08-07 19:01:23 +0200

    Initial Project Add

---
+++ src/main/java/com/example/springfwgurupetclinic/SpringFwGuruPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfwgurupetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFwGuruPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFwGuruPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springfwgurupetclinic/SpringFwGuruPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfwgurupetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFwGuruPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
