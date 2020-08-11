e6b77b3b7a209333496a9e334375ba9b982e237f
oliver <dr.oliver.lang@gmail.com>
2020-08-11 19:29:25 +0200

    Determine ID in storage layer

--- pet-clinic-data/src/main/java/springfwgurupetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/springfwgurupetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package springfwgurupetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import springfwgurupetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +16,16 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }
+        else {
+            throw new RuntimeException("Cannot save because object is null");
+        }
         return object;
     }
 
@@ -28,5 +35,9 @@ public abstract class AbstractMapService<T, ID> {
 
     void deleteById(ID id) {
         map.remove(id);
+    }
+
+    private Long getNextId() {
+        return map.isEmpty() ? 1L : Collections.max(map.keySet()) + 1L;
     }
 }
